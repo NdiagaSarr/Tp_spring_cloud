4f7c39996c855de539c61e49b74e05048386aff3
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-09-12 04:18:09 +0100

    Configuration de la route java et yml

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -9,5 +12,11 @@ public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
-
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route((r)->r.path("/customers/**").uri("http://localhost:8081/"))
+                .route((r)->r.path("/products/**").uri("http://localhost:8082/"))
+                .build();
+    }
 }
