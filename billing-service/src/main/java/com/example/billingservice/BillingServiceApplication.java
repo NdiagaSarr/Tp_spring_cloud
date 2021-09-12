b19cc8d02c410212737173fcb04d6700b5414411
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-09-12 07:24:43 +0100

    Associer les données avec le service billing

--- billing-service/src/main/java/com/example/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/com/example/billingservice/BillingServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.billingservice;
 
+import com.example.billingservice.entities.Bill;
+import com.example.billingservice.entities.ProductItem;
 import com.example.billingservice.feign.CustomerRestClient;
 import com.example.billingservice.feign.ProductRestClient;
 import com.example.billingservice.model.Customer;
+import com.example.billingservice.model.Product;
 import com.example.billingservice.repository.BillRepository;
 import com.example.billingservice.repository.ProductItemRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -10,8 +13,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.PagedModel;
 
-import java.util.Collection;
+import java.util.Date;
+import java.util.Random;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -28,7 +33,16 @@ public class BillingServiceApplication {
             System.out.println(customer.getId());
             System.out.println(customer.getName());
             System.out.println(customer.getEmail());
+            Bill bill=billRepository.save(new Bill(null,new Date(),null,customer.getId(),null));
+            PagedModel<Product> productPagedModel=productRestClient.pageProducts(0,10);
+            productPagedModel.forEach(p -> {
+                ProductItem productItem=new ProductItem();
+                productItem.setPrice(p.getPrice());
+                productItem.setQuantity(1+new Random().nextInt(100));
+                productItem.setBill(bill);
+                productItem.setProductID(p.getId());
+                productItemRepository.save(productItem);
+            });
         };
     }
-
 }
