9d19946d65d40cf16523c149705c10e349e804ba
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-09-12 03:11:27 +0100

    Ajout de l'entité produit et de son interface

---
+++ inventory-service/src/main/java/com/example/inventoryservice/entities/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.inventoryservice.entities.repository;
+
+import com.example.inventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
