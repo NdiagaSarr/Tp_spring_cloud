c1406d32f4b707736b443f26e825902318e2857d
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-09-12 04:50:26 +0100

    Rendre les services visible pour eurka et utiliser lb pour le gateway

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -12,11 +12,11 @@ public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
-    /*@Bean
+    @Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder){
         return builder.routes()
-                .route((r)->r.path("/customers/**").uri("http://localhost:8081/"))
-                .route((r)->r.path("/products/**").uri("http://localhost:8082/"))
+                .route((r)->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route((r)->r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
                 .build();
-    }*/
+    }
 }
