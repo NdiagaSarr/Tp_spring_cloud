3f2f78fe6b0cef9216b35e99310d872afecce426
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-09-12 01:36:44 +0100

    Création du projet

---
+++ src/main/java/com/example/tp_spring_cloud/TpSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_spring_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpSpringCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tp_spring_cloud/TpSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_spring_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
